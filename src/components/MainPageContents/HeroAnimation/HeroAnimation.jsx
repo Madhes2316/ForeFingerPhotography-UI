135d32ddccf404d604b58454c2008be399a79f8f
Madhes2316 <madhesmadhu2316@gmail.com>
2024-10-04 20:19:52 +0530

    Added Hero Cartoon and welcome message

--- src/components/MainPageContents/HeroAnimation/HeroAnimation.jsx
+++ src/components/MainPageContents/HeroAnimation/HeroAnimation.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import './HeroAnimation.css';
+import smileplease from '../../../assets/HeroAnimation/smile-please.png';
+import stars from '../../../assets/HeroAnimation/stars.svg';
 
 const HeroAnimation = () => {
   return (
-    <div className='heroAnimation-div'>
-        HeroAnimation
-    </div>
+      <div className='heroAnimation-text-div'>
+          <div className='welcometext-div'>
+            <p className='welcome-span'>Welcome to <span>ForeFinger</span> PhotoGraphy</p>
+            <div className='icon-quote-div'>
+              <img src={stars} alt="" />
+                <p className='ffpqoute-div'>
+                  Passing <span>smiles</span> for miles
+                </p>
+              <img src={stars} alt="" />
+            </div>
+          </div>
+          <div className='heroImage-div'>
+            <img src={smileplease} alt="" />
+          </div>
+      </div>
   )
 }
 
