1601bf33d94661e630ba51e2e605630d225f6fc0
Madhes2316 <madhesmadhu2316@gmail.com>
2024-10-06 11:27:55 +0530

    Added ClientList

--- src/components/MainPageContents/ClientList/ClientList.jsx
+++ src/components/MainPageContents/ClientList/ClientList.jsx
@@ -2,8 +2,32 @@ import React from 'react'
 import './ClientList.css';
 import boschlogo from '../../../assets/Clients-logo/Bosch-logo.png';
 import yathipumpslogo from '../../../assets/Clients-logo/yathi_pumps-logo.png';
+import algopumpslogo from '../../../assets/Clients-logo/algopumps-logo.png';
+import falconpumpslogo from '../../../assets/Clients-logo/falconpumps-logo.png';
 
 const ClientList = () => {
+    let companyListArray = [
+        {
+            id:1,
+            imgSrc:boschlogo,
+            companyName:'Bosch'
+        },
+        {
+            id:2,
+            imgSrc:yathipumpslogo,
+            companyName:'Yathi Pumps'
+        },
+        {
+            id:3,
+            imgSrc:algopumpslogo,
+            companyName:'Algo Pumps'
+        },
+        {
+            id:4,
+            imgSrc:falconpumpslogo,
+            companyName:'Falcon Pumps'
+        },
+    ]
   return (
     <div className='clientList-main-div'>
         <div className='ourClients-btntxt-div'>
@@ -11,30 +35,11 @@ const ClientList = () => {
             <span>Clients that we were able to Happily Serve</span>
         </div>
         <div className='clientBox-list-div'>
-            <div className='clientSpace-div'>
-                <img src={boschlogo} alt="" />
-            </div>
-            <div className='clientSpace-div'>
-                <img src={yathipumpslogo} alt="" />
-            </div>
-            <div className='clientSpace-div'>
-
-            </div>
-            <div className='clientSpace-div'>
-
-            </div>
-            <div className='clientSpace-div'>
-
-            </div>
-            <div className='clientSpace-div'>
-
-            </div>
-            <div className='clientSpace-div'>
-
-            </div>
-            <div className='clientSpace-div'>
-
-            </div>
+            {companyListArray.map(item => (
+                <div className='clientSpace-div'  key={item.id}>
+                    <img src={item.imgSrc} alt={item.companyName} />
+                </div>
+			))}
         </div>
     </div>
   )
