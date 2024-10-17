67379eb949bb8ab64d0dd5503e94b258135e03b3
Madhes2316 <madhesmadhu2316@gmail.com>
2024-10-17 21:46:02 +0530

    Added Gallery Thumbnail with css changes

--- src/components/GalleryPageContents/GalleryMainPage/GalleryMainPage.jsx
+++ src/components/GalleryPageContents/GalleryMainPage/GalleryMainPage.jsx
@@ -4,16 +4,34 @@ import galleryOneImg from '../../../assets/galleryPage/galleryOne-img.jpg';
 import galleryTwoImg from '../../../assets/galleryPage/galleryTwo-img.jpg';
 
 const GalleryMainPage = () => {
+
+  const onGalleryThumbnailClick = (galleryName)=>{
+    console.log(galleryName)
+  }
   return (
     <div className='galleryMainPage-div'>
-      <div className="selectGalleryText-div">
-        <span>Select Your Gallery</span>
-      </div>
-      <div className="galleryOne-div">
-          <img src={galleryOneImg} alt="Event and LifetStyle PhotoGraphy Thumbnail Image" />
-      </div>
-      <div className="galleryTwo-div">
-          <img src={galleryTwoImg} alt="Corporate and Industrial PhotoGraphy Thumbnail Image" />
+      <div className="galleryThumbnail-contents-div">
+          <div className="selectGalleryText-div">
+            <span>Select Your Gallery</span>
+          </div>
+          <div id="card-area">
+          <div className="wrapper">
+                <div className="box-area"> 
+                    <div className="box" onClick={()=> onGalleryThumbnailClick("galleryOne")}>
+                        <img src={galleryOneImg} alt="" />
+                        <div className="overlay">
+                            <h3>Event & LifeStyle Shoot</h3>
+                        </div>
+                    </div>
+                    <div className="box"  onClick={()=> onGalleryThumbnailClick("galleryTwo")}>
+                        <img src={galleryTwoImg} alt="" />
+                        <div className="overlay">
+                            <h3>Corp. & Industrial Shoot</h3>
+                        </div>
+                    </div>
+                </div>            
+          </div>       
+        </div>
       </div>
     </div>
   )
